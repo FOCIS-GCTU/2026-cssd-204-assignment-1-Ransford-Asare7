7cb84469d142248b1b817cfbcc4a3a6e12d588d6
Ransford-Asare7 <2425402984@live.gctu.edu.gh>
2026-06-01 23:47:04 +0100

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: initials.py
+# Description: Prints stylized large initials ROA with surrounding periods.
+# Assignment Number: 1
+#
+# Name: Ransford Nana Ofosu-Asare
+# STUDENT ID: 2425402984
+# Email: 2425402984@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Ransford Nana Ofosu-Asare, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    """Prints the small initials and stylized large initials for ROA."""
+    print()
+
+    print("...ROA")
+
+    print()
+
+    print("...RRRRRRRRRR...........OOOOOOOOOO..............AA..........")
+    print("...RRRRRRRRRRRR........OOOOOOOOOOOO............AAAA.........")
+    print("...RR........RR........OO........OO...........AA..AA........")
+    print("...RR........RR........OO........OO..........AA....AA.......")
+    print("...RRRRRRRRRRRR........OO........OO.........AA......AA......")
+    print("...RRRRRRRR............OO........OO........AAAAAAAAAAAA.....")
+    print("...RR....RR............OO........OO........AA........AA.....")
+    print("...RR......RR..........OO........OO........AA........AA.....")
+    print("...RR......RRRR...**...OOOOOOOOOOOO...**...AA........AA...**")
+    print("...RR........RR...**....OOOOOOOOOO....**...AA........AA...**")
+
+    print()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
